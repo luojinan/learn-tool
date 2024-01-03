4bccde1595c39e3e1d67724fb9144d21570026fe
luojinan <luojinan@zhuanzhuan.com>
2024-01-03 10:05:22 +0800

    feat: 引入unocss

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import './reset.css'
 import './style.css'
 import App from './App.vue'
+import 'uno.css'
 
 createApp(App).mount('#app')
 
--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import Unocss from 'unocss/vite'
+import { presetUno, presetAttributify, presetIcons } from 'unocss'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/learn-tool/',
-  plugins: [vue()],
+  plugins: [
+    vue(),
+    Unocss({
+      presets: [
+        presetUno(),
+        presetAttributify(),
+        presetIcons()
+      ],
+    })
+  ],
 })

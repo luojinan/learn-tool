443a2146013d3d49fe3568a0a2b0cf95b2f31055
luojinan <luojinan@zhuanzhuan.com>
2024-03-08 18:02:28 +0800

    temp

--- src/views/readgh/detail/index.vue
+++ src/views/readgh/detail/index.vue
@@ -1,7 +1,20 @@
 <script setup lang="ts">
 import { useFetch } from '@vueuse/core';
 import { useRouteQuery } from '@vueuse/router';
-import markdownit from 'https://esm.sh/markdown-it';
+import markdownit from 'https://esm.sh/markdown-it@14.0.0';
+// import markdownit from 'https://registry.npmmirror.com/markdown-it/14.0.0/files';
+// import markdownit from 'https://registry.npmmirror.com/markdown-it/14.0.0/files//dist/markdown-it.min.js';
+// https://esm.sh/markdown-it@14.0.0
+
+// esm 稍微有点慢，尝试使用cnpm，但是发现markdown-it产物mjs内部使用了裸引用
+// 猜测markdown-it的mjs产物专供本地构建工具使用
+// 强制本地安装前置依赖
+// 但是在esmsh会成功把前置依赖的裸引用转为esmsh地址，cnpm连这点小事的都不做？
+// ❌ esmsh 可以处理裸引用是因为加载的是github文件
+// 应该拿cnpm 和 unpkg对比，这类加载npm产物的cdn适用umd形式使用，而不是esm形式因为大部分npm产物的mjs都是给本地构建工具用的
+// 如 [通过 CDN 使用 Vue](https://cn.vuejs.org/guide/quick-start.html#using-vue-from-cdn)
+
+// 继续使用esmsh 尝试 1. 通过构建工具让加载加快 2. 使用预加载，在文章列表页选文章的空闲时间加载
 
 import { onMounted, ref } from 'vue';
 

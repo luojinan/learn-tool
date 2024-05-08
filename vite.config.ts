126ba63c56c1fe9c5dfc5c269bcbfdef6ff13c6c
luojinan <luojinan@zhuanzhuan.com>
2024-05-08 16:45:47 +0800

    feat: 引入eslint oxlint

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,25 @@
 // import { fileURLToPath, URL } from "node:url";
-import vue from '@vitejs/plugin-vue';
-import path from "path";
+import path from 'path'
+import vue from '@vitejs/plugin-vue'
 // import { visualizer } from "rollup-plugin-visualizer";
-import { defineConfig } from 'vite';
-const projectRootDir = path.resolve(__dirname);
+import { defineConfig } from 'vite'
+import Oxlint from 'unplugin-oxlint/vite'
+
+const projectRootDir = path.resolve(__dirname)
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/learn-tool/',
   plugins: [
     vue(),
+    Oxlint({
+      includes: ['src/**/*.{ts,vue}'],
+    }),
     // visualizer(),
   ],
   resolve: {
     alias: {
-      "@": path.resolve(projectRootDir,'src')
-    }
+      '@': path.resolve(projectRootDir, 'src'),
+    },
   },
 })

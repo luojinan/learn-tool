9ed2582e107cf40fef00b48b16dbb5212bb06196
luojinan <luojinan@zhuanzhuan.com>
2024-12-12 17:31:47 +0800

    build: 修改项目基础路径 - 将 Vite 配置中的 base 属性从 '/learn-tool/' 改为 './' - 此修改允许项目在任意路径下运行，提高了灵活性和兼容性

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/learn-tool/',
+  base: './',
   plugins: [
     vue(),
     Oxlint({
